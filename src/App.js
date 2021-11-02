c816db874d18a8bb69384d960d31fb4c0ba2a1f0
Jmz0127 <jmezz1@icloud.com>
2021-11-02 19:54:48 -0400

    imported state hook into app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 import '../src/components/Posts/Posts';
 import '../src/components/SearchBar/SearchBar';
