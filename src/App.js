fa5da0dd5e46413424192abacd44fe0b64113702
Jmz0127 <jmezz1@icloud.com>
2021-11-02 19:49:31 -0400

    imported dummy data

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import '../src/components/Posts/Posts';
 import '../src/components/SearchBar/SearchBar';
 
 // Import the dummyData
+import './dummy-data';
 import './App.css';
 
 const App = () => {
