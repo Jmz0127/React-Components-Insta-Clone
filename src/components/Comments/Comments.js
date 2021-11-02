02b64379a83b36363e7a96df78eef84b44c1b418
Jmz0127 <jmezz1@icloud.com>
2021-11-02 15:40:06 -0400

    updated .map on comments.js

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -4,13 +4,13 @@ import './Comments.css';
 
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
-  const { comments, changeStatus } = props;
+  const { comments } = props;
 
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
       {comments.map(com => {
-        return <Comment comment={com} changeStatus={changeStatus} key={com.id} />
+        return <Comment comment={com} key={com.text} />
       })}
     </div>
   );
